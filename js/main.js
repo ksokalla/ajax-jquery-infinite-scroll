4a092311261df3486c1fe44e68ed109cac527997
Krzysztof Sokalla <k.sokalla@gmail.com>
2017-02-12 15:39:04 +0100

    jquery infinite scroll finito

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 'use strict';
-
-
+$(document).ready(function () {
+	
+	$(window).scroll(function () {
+		
+		if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+			
+				$.getJSON('https://jsonplaceholder.typicode.com/users', function (data) {
+					
+										
+					$.each(data, function (key, val) {
+						$( "#sekcja" ).append("<p>User ID: " + val.id + "</p>");
+						$( "#sekcja" ).append("<p>User Name: " + val.name + "</p>");
+						$( "#sekcja" ).append("<p>User URL: " + val.website + "</p>" + "--------");
+					});
+				});
+			};
+	});
+});
